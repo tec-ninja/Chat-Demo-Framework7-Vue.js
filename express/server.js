cb34a912900f5b26530efadaf521d45e3d89e8fb
tec-ninja <devstar1112@gmail.com>
2021-09-07 21:48:50 +0800

    add netlify lambda func

---
+++ express/server.js
@@ -0,0 +1,57 @@
+'use strict';
+var serverless = require('serverless-http');
+var express = require('express');
+var app = express();
+// var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var moment = require('moment');
+var favicon = require('serve-favicon');
+
+app.set('port', (process.env.PORT || 3000));
+app.use(favicon('../public/icons/favicon.png'));
+app.use('/npm', express.static('../node_modules'));
+app.use(express.static('../build'));
+
+app.get('/', function(request, response) {
+    response.sendFile('../build/index.html');
+});
+
+// server.listen(app.get('port'), function() {
+//   console.log('Node app is running on port ', app.get('port'));
+// });
+
+var messages = [];
+var users = [];
+
+io.on('connection', function(socket) {
+
+    //users.push(socket.id);
+
+    socket.emit('init-chat', messages);
+    socket.emit('update-users', users);
+
+    socket.on('send-msg', function(data) {
+        var newMessages = data.map(item => {
+            item.timestamp = moment().format('dddd, MMMM D, HH:mm');
+            return item;
+        });
+        console.log('data', data);
+        messages.push(...newMessages);
+        socket.broadcast.emit('read-msg', newMessages);
+    });
+
+    socket.on('add-user', function(user){
+        users.push({ id: socket.id, name: user });
+        socket.emit('update-users', users);
+    });
+
+    socket.on('disconnect', function() {
+        users = users.filter(function(user){
+            return user.id != socket.id;
+        });
+        socket.emit('update-users', users);
+    });
+});
+
+module.exports = app;
+module.exports.handler = serverless(app);
